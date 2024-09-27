0482e6b61452a402921ee8837c52b592985c4d08
AliAlTaweel <engalihaitham@gmail.com>
2024-09-27 19:41:29 +0300

    fc

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/FS-P01-courseinfo-Ex1.3-1.5",
+  plugins: [react()],
+});
